59b7282ffcabe1e9cc9b2a25491916ba177d1927
Danny <danny.f.gomez@gmail.com>
2024-07-27 19:46:47 -0400

    create CRUD methods within message.repository

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,14 +1,32 @@
-import { readFile, writeFile } from "fs/promises";
+import { readFile, writeFile } from 'fs/promises';
 
 export class MessagesRepository {
+
+    // return a message by id
   async findOne(id: string) {
     const contents = await readFile('messages.json', 'utf8');
-    const messages = JSON.parse(contents)
+    const messages = JSON.parse(contents);
 
-    return messages[id]
+    return messages[id];
   }
 
-  async findAll() {}
+  // return all messages
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
 
-  async create(message: string) {}
+    return messages;
+  }
+
+  // create new message in json file
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+
+    const id = Math.floor(Math.random() * 999);
+
+    messages[id] = { id, content };
+
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
 }
