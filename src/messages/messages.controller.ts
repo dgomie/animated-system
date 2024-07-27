a356fdfeda045c0a91336d34cc0cbbb476992ed4
Danny <danny.f.gomez@gmail.com>
2024-07-26 23:43:00 -0400

    add pipe validation for post route

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
-
+import { CreateMessageDto } from './dtos/create-message-dto';
 //class decorator
 @Controller('messages')
 export class MessagesController {
@@ -9,7 +9,7 @@ export class MessagesController {
 
   @Post()
   //@Body is argument decorator, body: any is ts annotation
-  createMessage(@Body() body: any) {
+  createMessage(@Body() body: CreateMessageDto) {
     console.log(body);
   }
 
